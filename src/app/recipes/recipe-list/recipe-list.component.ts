87a7025af8bfc7da77d6b0a9b21674627150e9e8
Valeria Meza <valeriameza173@gmail.com>
2021-02-03 15:16:47 -0600

    add recipe list component model and render items.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'My recipe',
+      'A very good recipe',
+      'https://media.istockphoto.com/photos/cheesy-pepperoni-pizza-picture-id938742222?s=612x612'
+    ),
+    new Recipe(
+      'Another recipe',
+      'My mothers recipe',
+      'https://media.istockphoto.com/photos/cheesy-pepperoni-pizza-picture-id938742222?s=612x612'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
